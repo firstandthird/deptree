abf31c5b964d279976d634253983c223b39924aa
Greg Allen <me@jga.me>
2012-07-27 23:09:58 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+var DepTree = function() {
+  this.nodes = {}; 
+};
+
+DepTree.prototype.add = function(node, dependants) {
+  var self = this;
+
+  if (!dependants) {
+    dependants = [];
+  } else if (!(dependants instanceof Array)) {
+    dependants = [dependants];
+  }
+
+  this.nodes[node] = dependants;
+};
+
+
+DepTree.prototype.resolveNode = function(node, resolved, processing) {
+
+  var self = this;
+
+  resolved = resolved || [];
+  processing = processing || {};
+  var deps = this.nodes[node] || [];
+
+  processing[node] = true;
+
+  deps.forEach(function(dep) {
+    if (processing[dep]) {
+      throw new Error('Circular dependency: '+dep);
+    }
+    self.resolveNode(dep, resolved, processing);
+  });
+
+  if (resolved.indexOf(node) == -1) {
+    processing[node] = false;
+    resolved.push(node);
+  }
+  return resolved;
+};
+
+DepTree.prototype.resolve = function() {
+
+  var resolved = [];
+  for (var node in this.nodes) {
+    this.resolveNode(node, resolved);
+  }
+
+  return resolved;
+
+};
+
+
+module.exports = DepTree;
---
+++ test/deptree.test.js
@@ -0,0 +1,79 @@
+var assert = require('assert');
+var DepTree = require('../');
+
+
+suite('DepTree', function() {
+
+  var depTree;
+  setup(function() {
+    depTree = new DepTree();
+  });
+
+  test('no dependents', function() {
+    depTree.add('a');
+    var results = depTree.resolve();
+
+    assert.deepEqual(results, ['a']);
+  });
+
+  test('1 dependent', function() {
+    depTree.add('a', 'b');
+    var results = depTree.resolve();
+
+    assert.deepEqual(results, ['b', 'a']);
+  });
+
+  test('2 dependents', function() {
+    depTree.add('a', ['c', 'b']);
+    var results = depTree.resolve();
+
+    assert.deepEqual(results, ['c', 'b', 'a']);
+  });
+
+  test('add multiple', function() {
+    depTree.add('a');
+    depTree.add('b');
+    var results = depTree.resolve();
+
+    assert.deepEqual(results, ['a', 'b']);
+  });
+
+  test('no duplicates', function() {
+    depTree.add('a', 'b');
+    depTree.add('c', 'b');
+    var results = depTree.resolve();
+
+    assert.deepEqual(results, ['b', 'a', 'c']);
+  });
+
+  test('nested dependencies', function() {
+
+    depTree.add('a', ['b', 'd']);
+    depTree.add('b', ['c', 'd']);
+    var results = depTree.resolve();
+
+    assert.deepEqual(results, ['c', 'd', 'b', 'a']);
+  });
+
+  test('circular dependency check', function() {
+
+    depTree.add('a', 'b');
+    depTree.add('b', 'a');
+
+    assert.throws(function() {
+      depTree.resolve();
+    });
+  });
+
+  test('advanced circular dependency check', function() {
+
+    depTree.add('a', 'b');
+    depTree.add('b', 'c');
+    depTree.add('c', 'a');
+
+    assert.throws(function() {
+      depTree.resolve();
+    });
+  });
+
+});
